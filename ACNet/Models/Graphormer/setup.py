8ff8bdfdb5ce7127aa8e486850221d737dcdd52e
ZiqiaoZhang <49937476+ZiqiaoZhang@users.noreply.github.com>
2022-06-16 15:20:34 +0800

    Delete ACNet directory

--- ACNet/Models/Graphormer/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-setup(name='Alogs app',
-      ext_modules=cythonize('algos.pyx'),
-      include_dirs=[numpy.get_include()])
